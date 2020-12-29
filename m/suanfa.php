be91223f770778ddaf101934e95fa7ca466c9078
卢晓峰 <luxiaofeng.200@163.com>
2020-12-29 17:11:15 +0800

    RD

--- m/suanfa.php
+++ m/suanfa.php
@@ -77,6 +77,88 @@ function king($n , $m){
         return $monkey[0];
     }else{
         return false;
-        return false;
     }
+}
+
+#快速排序的方法
+function quickSort($list=[]){
+    if(!$list)
+        return false;
+    $baseNum = $list[0];
+    $leftArray = $rightArray = [];
+    for($i = 0;$i <count($list);$i++){
+        if($baseNum[$i]>$list[$i]){
+            $leftArray[] = $list[$i];
+        }else{
+            $rightArray[] = $list[$i];
+        }
+    }
+    $leftArray = quickSort($leftArray);
+    $rightArray = quickSort($rightArray);
+    return array_merge($leftArray,array($baseNum),$rightArray);
+}
+
+// 冒泡排序
+function bubble_sort(&$arr){
+    if(!$arr) return [];
+    for ($i=0,$len=count($arr); $i < $len; $i++) {
+        for ($j=1; $j < $len-$i; $j++) {
+            if ($arr[$j-1] > $arr[$j]) {
+                $temp = $arr[$j-1];
+                $arr[$j-1] = $arr[$j];
+                $arr[$j] = $temp;
+            }
+        }
+    }
+}
+
+//输入一串字符，并按照顺序进行排序
+function strSort($str){
+    $arr = str_split($str);
+    $res = array_flip(array_flip($arr));
+    sort($res);
+    return $res;
+}
+
+
+//遍历文件夹
+function listDir($dir =''){
+    if(!$dir)
+        return false;
+    $files = [];
+    if($handle =opendir($dir)){
+        //读取文件夹判断
+        while ($file =readdir($dir)!==false){
+            if(is_dir($file)){
+                if($file!='.' && $file!='..'){
+                    $files[]=listDir($dir.'/'.$file);
+                }else{
+                    $files[] = $file;
+                }
+            }
+        }
+        closedir($handle);//关闭目录
+    }
+    return $files;
+}
+
+#递归无限分类的主要处理过程
+function tree($arr=[],$pid=0,$level=0){
+    static $cateArr = [];
+    foreach ($arr as $v) {
+        if($v['pid'] == $pid){
+            $v['level'] = $level;
+            $cateArr[] = $v;
+            tree($arr, $v['id'] , $level+1);
+        }
+    }
+    return $cateArr;
+}
+#利用条件运算符的嵌套来完成此题：学习成绩> =90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示。
+function actionGetScore($score=''){
+    $score = (int) $score;
+    if($score<0 || $score>100)
+        return false;
+    $result = $score>=90 ?'A':($score>=60 ? 'B' : 'C');
+    return  $result;
 }
