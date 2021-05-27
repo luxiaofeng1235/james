34373a7953a536d0c86af59f5c6b5ac95b10ad1f
卢晓峰 <luxiaofeng.200@163.com>
2021-05-27 13:41:29 +0800

    优化

--- aa.php
+++ aa.php
@@ -1,231 +1,60 @@
 <?php
+/**
+ * des-ecb加密
+ * @param string  $data 要被加密的数据
+ * @param string  $key 加密密钥(64位的字符串)
+ */
+function des_ecb_encrypt($data, $key){
+    return openssl_encrypt ($data, 'des-ecb', $key);
+}
 
-
-
-// $tt = "test\r\n";
-// $tt.="1122222\r\n";
-// $tt.='33344';
-
- // =file_put_contents('sites.txt', $tt,FILE_APPEND);
-// echo '<pre>';
-// var_dump($aa);
-// echo '</pre>';
-// exit;
-
-
-//https://blog.csdn.net/zhemejinnameyuanxc/article/details/83383434
-ini_set("limit_memory",'8000M');
-set_time_limit(600);
-//获取当前的路径位置
-$dirname = dirname(__FILE__); //返回根目录
-$dirname = str_replace('\\', '/', $dirname);
-// require_once ($dirname."/library/ThreeDesUtil.php");
-      
-class AES
-{
-   public static function encrypt($data, $key) {
-       $data =  openssl_encrypt($data, 'aes-128-ecb', base64_decode($key), OPENSSL_RAW_DATA);
-       return base64_encode($data);
-   }
-
-    public static function decrypt($data, $key) {
-      $encrypted = base64_decode($data);
-      return openssl_decrypt($encrypted, 'aes-128-ecb', base64_decode($key), OPENSSL_RAW_DATA);
-    }
+/*
+Array(
+0 => aes-128-cbc,   // aes加密
+1 => des-ecb,       // des加密
+2 => des-ede3,      // 3des加密
+...
+)
+*/
+ 
+/**
+ * des-ecb解密
+ * @param string  $data 加密数据
+ * @param string  $key 加密密钥
+ */
+function des_ecb_decrypt ($data, $key){
+    return openssl_decrypt ($data, 'des-ecb', $key);
 }
 
 
+$ret= '%7B%22PayInfo%22%3A%7B%22CurrencyCode%22%3A%22156%22%2C%22AccNo%22%3A%226225880161919122%22%2C%22AccName%22%3A%22%E9%8D%97%E3%88%A1%E6%AA%BD%E5%AE%84%EF%BF%BD%22%2C%22AccType%22%3A1%2C%22TxnAmt%22%3A100%7D%2C%22TermInfo%22%3A%7B%22TermID%22%3A%22DF000298%22%7D%2C%22TxnInfo%22%3A%7B%22TxnTime%22%3A%22154317%22%2C%22TxnDate%22%3A%2220210525%22%2C%22OrderID%22%3A%2220210525154317773666%22%7D%2C%22MerInfo%22%3A%7B%22ChlID%22%3A%22010113%22%2C%22MerID%22%3A%22333110348160198%22%2C%22MerName
+        %22%3A%22%E5%A8%B4%E5%AC%AD%E7%98%AF%22%7D%2C%22VerInfo%22%3A%7B%22VerInter%22%3A%222.0%22%7D%2C%22TxnNo%22%3A%22PAY201%22%2C%22Signature%22%3A%22YTVMMJA1MJNIYJZIODQWZJQ4MDFLYZLKMDAZZGIYYTI=%22%7D';
+
+echo '<pre>';
+print_R(urldecode($ret));
+echo '</pre>';
+exit;
+$a=openssl_get_cipher_methods();
+echo '<pre>';
+var_dump($a);
+echo '</pre>';
+exit;
 $data = '123456';//加密明文
-$method = 'aes-128-ecb';//加密方法 :对应JAVA的AES/ECB/PKCS5Padding算法
 $passwd = 'udik876ehjde32dU61edsxsf';//加密密钥
 $options = 0;//数据格式选项（可选）
 $iv = '';//加密初始化向量（可选）
 
-$aa= AES::encrypt($data,$passwd);
+$aa= des_ecb_encrypt($data,$passwd);
+
+$code = bin2hex($aa);
 echo "data源数据：".$data;
 echo "<br/>";
 echo "通讯秘钥：".$passwd;
 echo "<br/>";
 echo "加密后的数据：".$aa;
-$arr= AES::decrypt($aa,$passwd);
+// $aaa= pack("H*",bin2hex($aa)); 
+$jiemi= des_ecb_decrypt($aa,$passwd);
 echo "<br/>";
-echo "通过加密后反解密的：".$arr;
-exit;
-// echo '<pre>';
-// print_R($aa);
-// echo '</pre>';
-// exit;
-
-echo "原始串为data：".$data;
-echo "<hr/>";
-
-$result = openssl_encrypt($data, $method, $passwd, OPENSSL_RAW_DATA);
-$code=base64_encode($result);
-echo "加密后的串为：".$code;
-echo "<br/>";
- 
-
-$jiemi =openssl_decrypt($result, $method, $passwd,OPENSSL_RAW_DATA);
-echo "解密后为：".$jiemi;
-echo "<br/>";exit;
-
-//解密：
-
-// $iput='123456';
-// $key='123';
-// $data = openssl_encrypt($input,'des-ede3',$key,0);
-// $data= base64_decode($data);
-
-// $miwen =$data;
-
-// #解密
-// $decrypted = openssl_decrypt(base64_decode($miwen),'des-ede3',$key,OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING);
-// echo '<pre>';
-// var_dump($decrypted);
-// echo '</pre>';
-// exit;
-
-$key  = 'ABCEDFGHIJKLMNOPQ';
-$iv  = '0123456789';
-$des = new Encrypt($key, $iv);
-$str = "abcdefghijklmnopq";
-echo "source: {$str},len: ",strlen($str),"\r\n";
-$e_str = $des->encrypt3DES($str);
-echo "entrypt: ".$e_str, "\r\n";
-exit;
-$res = ThreeDesUtil::Encrypt();
-echo '<pre>';
-var_dump($res);
-echo '</pre>';
-exit;
-function getDatas($content = ''){
-      global $dirname;
-      require_once ($dirname."/library/phpanalysis/phpanalysis.class.php");
-      $pa=new PhpAnalysis();
-      $pa->SetSource($content);
-      $pa->resultType=2;
-      $pa->differMax=true;
-      $pa->StartAnalysis();
-      $arr=$pa->GetFinallyIndex();
-      $splitVal =  array_keys($arr);
-      if($splitVal[0]  && in_array($splitVal,['广州','东莞']) ) {
-            return $splitVal[0].$splitVal[1];
-      }if(strpos($splitVal[0], '银行')!=false){
-            return $splitVal[0];
-      }else if(isset($splitVal[1]) && $splitVal[1] =='银行'){
-            return $splitVal[0].$splitVal[1];
-      }else{
-            return $splitVal[0];
-      }
-}
-
- 
-  $arr = getDatas('大连银行');
-
-//   //判断
-echo '<pre>';
-print_R($arr);
-echo '</pre>';
-exit;
-
-
-
-
- 
-
-// $str ='151515|2
-// 140400001|10000000016327|0|100|测试收款人名称|测试行名称|123456789123|6236681300001758483|附言1
-// 140400002|10000000016327|0|100|测试收款人名称|测试行名称|123456789123|6236681300001758483|附言2';
-// echo '<pre>';
-// var_dump($str);
-// echo '</pre>';
-// echo "md5后的内容：";
-// echo "<br/>";
-
-// $wai_str= md5($str);
-// echo $wai_str;
-// echo "<br/>";
-// $aaa =encrypt_sha256($wai_str);
-
-// echo encrypt_sha256('b205dd291f1c7117a6f895a97bc8a943');
-// echo "<hr/>";
-// echo "md5变为sha256后：".$aaa;
-// exit;
-
-
- 
-$time = date('Y-m-d H:i:s');
-$merOrderId = '319420170832021042842'; //处理生成的订单号
-$str = 'divisionFlag=true&goods=[{"body":"goods body","goodsCategory":"Auto","goodsId":"0001","goodsName":"电费","price":100,"quantity":1,"subMerchantId":"988460101800202","subOrderAmount":1},{"body":"goods body","goodsCategory":"Auto","goodsId":"0002","goodsName":"电费","price":200,"quantity":2,"subMerchantId":"988460101800201","subOrderAmount":2}]&instMid=MINIDEFAULT&merOrderId='.$merOrderId.'&mid=898201612345678&msgSrc=WWW.TEST.COM&msgType=wx.unifiedOrder&orderDesc=账单描述&platformAmount=0&requestTimestamp='.$time.'&signType=SHA256&tid=00000001&totalAmount=3fcAmtnx7MwismjWNhNKdHC44mNXtnEQeJkRrhKJwyrW2ysRR';
-
-echo "待验签参数：".$str;
-echo "<hr/>";
-
-
-        $res=[
-            // 'divisionFlag' =>true,
-            // 'goods'=>[
-            //         [
-            //             'body'=>'goods body',
-            //             'goodsCategory'=>'113,122',
-            //             'goodsId'=>'0001',
-            //             'goodsName'=>'电费1',
-            //             'price'=>'100',
-            //             'quantity'=>'1',
-            //             'subMerchantId'=>'988460101800202',
-            //             'subOrderAmount'=>'1',
-            //         ],
-            //         [
-            //             'body'=>'goods body',
-            //             'goodsCategory'=>'113,122',
-            //             'goodsId'=>'0002',
-            //             'goodsName'=>'电费2',
-            //             'price'=>'200',
-            //             'quantity'=>'2',
-            //             'subMerchantId'=>'988460101800234',
-            //             'subOrderAmount'=>'2',
-            //         ],
-            // ],
-            'instMid'=>'MINIDEFAULT',
-            'merOrderId'=>'319420170832021042746',
-            'mid'=>'898201612345678',
-            'msgSrc'=>'WWW.TEST.COM',
-            'msgType'=>'wx.unifiedOrder',
-            'signType'=>'SHA256',
-            'orderDesc'=>'账单描述',
-            // 'platformAmount'=>'0',
-            'requestTimestamp'=>date('Y-m-d H:i:s'),
-            'signType'=>'SHA256',
-            'tid'   =>  '00000001',
-            'totalAmount'=>'300',
-
-        ];
-
-
-//55116142f6904c040ff83173913b2dd0ecde8314a68bcdc74cc31d3228189545
-$sign = encrypt_sha256($str);
-echo '生成的秘钥:'.$sign;
-echo "<br/>";
-echo "<br/>";
-
-
-
-
-$postdata= '{"mid":"898201612345678","tid":"00000001","msgType":"wx.unifiedOrder","msgSrc":"WWW.TEST.COM","instMid":"MINIDEFAULT","signType":"SHA256","orderDesc":"账单描述","merOrderId":"'.$merOrderId.'","totalAmount":3,"requestTimestamp":"'.$time.'","divisionFlag":true,"platformAmount":0,"goods":[{"body":"goods body","goodsCategory":"Auto","goodsId":"0001","goodsName":"电费","price":100,"quantity":1,"subMerchantId":"988460101800202","subOrderAmount":1},{"body":"goods body","goodsCategory":"Auto","goodsId":"0002","goodsName":"电费","price":200,"quantity":2,"subMerchantId":"988460101800201","subOrderAmount":2}],"sign":"'.$sign.'"}';
-
-echo '发送的报文：'.$postdata;
-echo "<hr/>";
- 
-
-$url = 'https://qr-test2.chinaums.com/netpay-route-server/api/';
- 
-
- $initdata = webRequest($url,'POST',$postdata);
- echo '<pre>';
- print_R($initdata);
- echo '</pre>';
- exit;
-
-
+echo "通过加密后反解密的：".$jiemi;
 
 ?>
