83dfb2e7a377c79c578d1b9e5128037cd7c33239
卢晓峰 <luxiaofeng.200@163.com>
2024-02-19 17:44:15 +0800

    12

--- request/book_detail.php
+++ request/book_detail.php
@@ -49,7 +49,16 @@ if($info){
 《<a href="/xiaoshuo/RenZaiHeHuanZong_NiRangWoShouShenRuYu_.html" title="人在合欢宗，你让我守身如玉？">人在合欢宗，你让我守身如玉？</a>》是网络小说作家<a href="/author/NanFengGuoJing.html" target="_blank" title="南风过境作品集">南风过境</a>创作的长篇玄幻奇幻小说<br><p>苏尘因意外穿越到了天元大世界，并且还成为了合欢宗的一名弟子！最关键的是苏尘身为九阳圣体，在合欢宗是绝佳的鼎炉，毫无疑问他成为了无数合欢宗女性的梦中情人！四位拥有绝世容颜，性格不一样的师姐都对他青睐有加，每天都渴望于他双修。就连宗门内的师妹也是百般尝试，倾心于他。然而就在苏尘准备提枪上战场的时候，守身如玉系统突然降临。【滴】【宿主可通过守身如玉来获取奖励】【任务一：不与师姐双修，奖励一重境界】【任务二：与师姐双修，惩罚宿主渡雷劫】苏尘欲哭无泪道。“我都穿越到合欢宗了，你让我守身如玉？”</p><p>您要是觉得</p><a href="https://www.souduw.com" title="搜读网">搜读网</a>提供<a href="/xiaoshuo/RenZaiHeHuanZong_NiRangWoShouShenRuYu_.html" title="人在合欢宗，你让我守身如玉？">人在合欢宗，你让我守身如玉？</a>免费阅读(<a href="https://www.souduw.com/xiaoshuo/RenZaiHeHuanZong_NiRangWoShouShenRuYu_.html" title="https://www.souduw.com/xiaoshuo/RenZaiHeHuanZong_NiRangWoShouShenRuYu_.html">https://www.souduw.com/xiaoshuo/RenZaiHeHuanZong_NiRangWoShouShenRuYu_.html</a>)</div>
 <div id="listtj">推荐阅读：<a href="https://www.souduw.com/xiaoshuo/YiJianDuZun.html" style="color: #c00;">一剑独尊</a>、<a href="https://www.souduw.com/xiaoshuo/ChangShengWuDao_CongWuQinYangShengQuanKaiShi.html" style="color: #c00;">长生武道：从五禽养生拳开始</a>、<a href="https://www.souduw.com/xiaoshuo/DaoGuiYiXian.html" style="color: #c00;">道诡异仙</a>、<a href="https://www.souduw.com/xiaoshuo/BuKeXueYuShou.html" style="color: #c00;">不科学御兽</a>、<a href="https://www.souduw.com/xiaoshuo/TianQiYuBao.html" style="color: #c00;">天启预报</a>、<a href="https://www.souduw.com/xiaoshuo/ShenKongBiAn.html" style="color: #c00;">深空彼岸</a>、<a href="https://www.souduw.com/xiaoshuo/KaiJuQianDaoHuangGuShengTi.html" style="color: #c00;">开局签到荒古圣体</a>、<a href="https://www.souduw.com/xiaoshuo/GaoTianZhiShang.html" style="color: #c00;">高天之上</a>、<a href="https://www.souduw.com/xiaoshuo/BaoHuWoFangZuChang.html" style="color: #c00;">保护我方族长</a>、<a href="https://www.souduw.com/xiaoshuo/WoDeGuiYiRenShengMoNiQi.html" style="color: #c00;">我的诡异人生模拟器</a>、<a href="https://www.souduw.com/xiaoshuo/HeDongLiJianXian.html" style="color: #c00;">核动力剑仙</a>、<a href="https://www.souduw.com/xiaoshuo/ZuiChuJinHua.html" style="color: #c00;">最初进化</a>、<a href="https://www.souduw.com/xiaoshuo/YeDeMingMingShu.html" style="color: #c00;">夜的命名术</a>、<a href="https://www.souduw.com/xiaoshuo/XiuLianCongJianHuaGongFaKaiShi.html" style="color: #c00;">修炼从简化功法开始</a>、<a href="https://www.souduw.com/xiaoshuo/MinSuCongXiangXiXueShenKaiShi.html" style="color: #c00;">民俗从湘西血神开始</a>、<a href="https://www.souduw.com/xiaoshuo/WuXingManJi_JianGeGuanJianLiuShiNian.html" style="color: #c00;">悟性满级：剑阁观剑六十年</a>、<a href="https://www.souduw.com/xiaoshuo/GuaiTanWanJia.html" style="color: #c00;">怪谈玩家</a>、<a href="https://www.souduw.com/xiaoshuo/ZheGeWuShengGuoYuKangKai.html" style="color: #c00;">这个武圣过于慷慨</a>、<a href="https://www.souduw.com/xiaoshuo/HaiLanSaLingZhu.html" style="color: #c00;">海兰萨领主</a>、<a href="https://www.souduw.com/xiaoshuo/MingKeJie13Hao.html" style="color: #c00;">明克街13号</a>、</div>
 </div>
-</div>';
+</div>
+<div class="mulu">
+<h2>《<a href="" title="重生之青云直上">重生之青云直上</a>》章节列表</h2>
+<ul>
+<li><a href="/ChongShengZhiQingYunZhiShang/1_1.html">第1章 大梦一场，我捏着满手王炸重生了</a></li>
+<li><a href="/ChongShengZhiQingYunZhiShang/2_1.html">第2章 我想要这个人</a></li>
+<li><a href="/ChongShengZhiQingYunZhiShang/3_1.html">第3章 这人胆子很大啊</a></li>
+</ul>
+</div>
+';
 
         //$detail = webRequest($link_url , 'GET' , [],[]);
         preg_match("/<div class=\"jieshao\".*?>.*?<\/div>/ism",$detail,$matchesRes);
@@ -96,14 +105,32 @@ if($info){
         $html = str_replace('</a>','',$html);
         $html =str_replace('</em>','' ,$html);
         $store_data['ims_novel_info'] = $html;
-
-        echo '<pre>';
-        print_R($store_data);
-        echo '</pre>';
-        exit;
     }
 
 
+    $chapter_detal  = [];
+    //匹配目录信息
+    preg_match("/<div class=\"mulu\".*?>.*?<\/div>/ism",$detail,$matchesRes);
+    if(isset($matchesRes[0]) && !empty($matchesRes[0])){
+        $pat = '/href=[\"|\'](.*?)[\"|\']/i';
+        $newdata = preg_match_all("/<li.*?>.*?<\/li>/ism" , $matchesRes[0],$aaa);
+
+        if(!empty($aaa)){
+            foreach($aaa[0] as $link_value){
+                preg_match($pat , $link_value ,$link_info);
+                $chapter_detal[]=$link_info[1] ?? '';
+            }
+        }
+        echo '<pre>';
+        print_R($chapter_detal);
+        echo '</pre>';
+        exit;
+        // preg_match_all($pat , $matchesRes[0],$chapterData);//获取章节信息
+        echo '<pre>';
+        print_R($chapterData);
+        echo '</pre>';
+        exit;
+    }
     }
 }else{
     echo "no data";
