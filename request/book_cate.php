6b4f5ea1e144d80f9972742c4c07dac5bcadb301
卢晓峰 <luxiaofeng.200@163.com>
2024-02-19 15:43:40 +0800

    12

--- request/book_cate.php
+++ request/book_cate.php
@@ -16,11 +16,9 @@ $dirname =str_replace("\\", "/", $dirname) ;
 require_once($dirname.'/library/init.inc.php');
 
 
-// $url = 'https://www.souduw.com/'; //需要爬取的url章节信息
-
-
+$url = 'https://www.souduw.com/'; //需要爬取的url章节信息
 // $data = webRequest($url ,'GET',[],[]);//获取当前的匹配的内容信息
-$data ='<div class="proxylistitem">
+$data ='<div class="lastupdate">
       <div style="float:left; display:block; width:630px;">
 　　　　　 <li>
             <span class="lx"><a href="/youxijingji.html" title="游戏竞技">游戏竞技</a></span>
@@ -48,7 +46,7 @@ $data ='<div class="proxylistitem">
 if($data){
     $table_name= 'ims_category';//需要插入的表名称
     //<div class="lastupdate"> 最新更新
-    preg_match("/<div class=\"proxylistitem\".*?>.*?<\/div>/ism",$data,$matchesRes);
+    preg_match("/<div class=\"lastupdate\".*?>.*?<\/div>/ism",$data,$matchesRes);
     if(isset($matchesRes[0]) && !empty($matchesRes[0])){
         $nearby_item = $matchesRes[0] ?? [];
         //获取分类的基础信息
@@ -58,6 +56,15 @@ if($data){
                 //--- 导出站外链接
                 $content_info = $matchesRes_cate[0] ?? [];
                 $pat = '/href=[\"|\'](.*?)[\"|\']/i';
+                //<span class="sm">
+                preg_match_all("/<span class=\"sm\".*?>.*?<\/span>/ism" , $nearby_item,$art_list);
+                $book_all = [];
+                if(isset($art_list[0]) && $art_list[0]){
+                    foreach($art_list[0] as $value){
+                        preg_match($pat,$value,$art_info);
+                        $book_all[]['article_url'] = $art_info[1] ?? '';
+                    }
+                }
                 // echo '<pre>';
                 // print_R($content_info);
                 // echo '</pre>';
@@ -72,29 +79,49 @@ if($data){
                     foreach($content_info as $response){
                         preg_match($pat, $response, $link_data);//获取当前的链接
                         preg_match($en_preg , $response,$title_data);//正则匹配汉字
+
+                        //
+                        // //防止重复插入
+                        $items[] = [
+                            'cate_name'     =>  $title_data[0] ?? '',
+                            'link_url'  =>  $link_data[1] ?? '',
+                            'createtime'    => $now_time,
+                        ];
+                    }
+
+                    // if(!empty($items)){
+                    //     $result = $mysql_obj->add_data($items ,$table_name);
+                    //     if(!$result){
+                    //         echo "complate error";
+                    //     }
+                    //     echo "同步分类完成";
+                    // }else{
+                    //     echo "hava complate category data";
+                    // }
+                }
+                if($book_all){
+                    $cate_data  = [];
+                    foreach($book_all as $key =>$val){
+                        $baseinfo = $items[$key]?? [];
+                        $detail = array_merge($val , $baseinfo);
                         //匹配当前是否存在已存在的连接
-                        $where_data = "link_url = '".$link_data[1]."'";
-                        $info = $mysql_obj->get_data_by_condition($where_data,$table_name,'cate_name');
-                        //防止重复插入
+                        $where_data = "article_url = '".$detail['article_url']."'";
+                        $info = $mysql_obj->get_data_by_condition($where_data,$table_name,'id');
                         if(!$info){
-                             $items[] = [
-                                'cate_name'     =>  $title_data[0] ?? '',
-                                'link_url'  =>  $link_data[1] ?? '',
-                                'createtime'    => $now_time,
-                            ];
+                            $cate_data[] =$detail;
                         }
                     }
-                    if(!empty($items)){
-                        $result = $mysql_obj->add_data($items ,$table_name);
+                    unset($items);
+                    unset($book_all);
+                    if($cate_data){
+                        $result = $mysql_obj->add_data($cate_data ,$table_name);
                         if(!$result){
                             echo "complate error";
                         }
-                        echo "同步分类完成";
+                         echo "最新文章同步完成";
                     }else{
-                        echo "hava complate category data";
+                        echo "have complate";
                     }
-                }else{
-                    echo "no data ";//已经同步
                 }
         }else{
             echo 'no category data !!!';
