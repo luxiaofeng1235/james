1a77bcc8024e7b2a66d941f2ed17cc8b4576e346
卢晓峰 <luxiaofeng.200@163.com>
2024-02-19 15:50:53 +0800

    修改

--- request/book_cate.php
+++ request/book_cate.php
@@ -17,32 +17,32 @@ require_once($dirname.'/library/init.inc.php');
 
 
 $url = 'https://www.souduw.com/'; //需要爬取的url章节信息
-// $data = webRequest($url ,'GET',[],[]);//获取当前的匹配的内容信息
-$data ='<div class="lastupdate">
-      <div style="float:left; display:block; width:630px;">
-　　　　　 <li>
-            <span class="lx"><a href="/youxijingji.html" title="游戏竞技">游戏竞技</a></span>
-            <span class="sm"><a href="/xiaoshuo/LianMeng_KaiJuSuoNa_SongZouLiaoZhouJieDaiMei.html" title="联盟：开局唢呐，送走了周姐呆妹">联盟：开局唢呐，送走了周姐呆妹</a></span>
-            <span class="zj"><a href="/LianMeng_KaiJuSuoNa_SongZouLiaoZhouJieDaiMei/822_1.html" title="联盟：开局唢呐，送走了周姐呆妹 第785章 用峡谷先锋拖延北虎" target="_blank">第785章 用峡谷先锋拖延北虎</a></span>
-            <span class="zz"><a href="/author/YouYingYeShen2.html" target="_blank" title="幽影夜神2">幽影夜神2</a></span>
-            <span class="sj">02-19</span>
-            </li>
-            <li>
-            <span class="lx"><a href="/dushishenghuo.html" title="都市生活">都市生活</a></span>
-            <span class="sm"><a href="/xiaoshuo/LongYi_KaiJuCanZaoBeiPan_FanShouYingQuXiaoYiZi.html" title="龙医：开局惨遭背叛，反手迎娶小姨子">龙医：开局惨遭背叛，反手迎娶小姨子</a></span>
-            <span class="zj"><a href="/LongYi_KaiJuCanZaoBeiPan_FanShouYingQuXiaoYiZi/275_1.html" title="龙医：开局惨遭背叛，反手迎娶小姨子 第二百七十五章 无名剑出！" target="_blank">第二百七十五章 无名剑出！</a></span>
-            <span class="zz"><a href="/author/ZuiXingChen4.html" target="_blank" title="罪星辰">罪星辰</a></span>
-            <span class="sj">02-19</span>
-            </li>
-            <li>
-<span class="lx"><a href="/dushishenghuo.html" title="都市生活">都市生活</a></span>
-<span class="sm"><a href="/xiaoshuo/ShangChengZhiXia.html" title="上城之下">上城之下</a></span>
-<span class="zj"><a href="/ShangChengZhiXia/301_1.html" title="上城之下 第0301章 惊人发现" target="_blank">第0301章 惊人发现</a></span>
-<span class="zz"><a href="/author/LiMa.html" target="_blank" title="李马">李马</a></span>
-<span class="sj">02-19</span>
-</li>
-　　　　</div>
-</div>';
+$data = webRequest($url ,'GET',[],[]);//获取当前的匹配的内容信息
+// $data ='<div class="lastupdate">
+//       <div style="float:left; display:block; width:630px;">
+// 　　　　　 <li>
+//             <span class="lx"><a href="/youxijingji.html" title="游戏竞技">游戏竞技</a></span>
+//             <span class="sm"><a href="/xiaoshuo/LianMeng_KaiJuSuoNa_SongZouLiaoZhouJieDaiMei.html" title="联盟：开局唢呐，送走了周姐呆妹">联盟：开局唢呐，送走了周姐呆妹</a></span>
+//             <span class="zj"><a href="/LianMeng_KaiJuSuoNa_SongZouLiaoZhouJieDaiMei/822_1.html" title="联盟：开局唢呐，送走了周姐呆妹 第785章 用峡谷先锋拖延北虎" target="_blank">第785章 用峡谷先锋拖延北虎</a></span>
+//             <span class="zz"><a href="/author/YouYingYeShen2.html" target="_blank" title="幽影夜神2">幽影夜神2</a></span>
+//             <span class="sj">02-19</span>
+//             </li>
+//             <li>
+//             <span class="lx"><a href="/dushishenghuo.html" title="都市生活">都市生活</a></span>
+//             <span class="sm"><a href="/xiaoshuo/LongYi_KaiJuCanZaoBeiPan_FanShouYingQuXiaoYiZi.html" title="龙医：开局惨遭背叛，反手迎娶小姨子">龙医：开局惨遭背叛，反手迎娶小姨子</a></span>
+//             <span class="zj"><a href="/LongYi_KaiJuCanZaoBeiPan_FanShouYingQuXiaoYiZi/275_1.html" title="龙医：开局惨遭背叛，反手迎娶小姨子 第二百七十五章 无名剑出！" target="_blank">第二百七十五章 无名剑出！</a></span>
+//             <span class="zz"><a href="/author/ZuiXingChen4.html" target="_blank" title="罪星辰">罪星辰</a></span>
+//             <span class="sj">02-19</span>
+//             </li>
+//             <li>
+// <span class="lx"><a href="/dushishenghuo.html" title="都市生活">都市生活</a></span>
+// <span class="sm"><a href="/xiaoshuo/ShangChengZhiXia.html" title="上城之下">上城之下</a></span>
+// <span class="zj"><a href="/ShangChengZhiXia/301_1.html" title="上城之下 第0301章 惊人发现" target="_blank">第0301章 惊人发现</a></span>
+// <span class="zz"><a href="/author/LiMa.html" target="_blank" title="李马">李马</a></span>
+// <span class="sj">02-19</span>
+// </li>
+// 　　　　</div>
+// </div>';
 if($data){
     $table_name= 'ims_category';//需要插入的表名称
     //<div class="lastupdate"> 最新更新
@@ -66,10 +66,9 @@ if($data){
                     }
                 }
                 // echo '<pre>';
-                // print_R($content_info);
+                // print_R($book_all);
                 // echo '</pre>';
                 // exit;
-                // preg_match_all($pat, $matchesRes_cate[0], $content_hrefs);
                 //摘取连接里的数据信息
                 $items = [];
                 # $en_pa ='/[^\u4e00-\u9fa5]/';
@@ -88,16 +87,6 @@ if($data){
                             'createtime'    => $now_time,
                         ];
                     }
-
-                    // if(!empty($items)){
-                    //     $result = $mysql_obj->add_data($items ,$table_name);
-                    //     if(!$result){
-                    //         echo "complate error";
-                    //     }
-                    //     echo "同步分类完成";
-                    // }else{
-                    //     echo "hava complate category data";
-                    // }
                 }
                 if($book_all){
                     $cate_data  = [];
@@ -128,7 +117,6 @@ if($data){
         }
 
     }
-    # preg_match_all('/<div\sstyle=\"float[^>]+>.*?<\/div>/',$line,$out);
 }else{
     echo 'no data now!@!';
 }
