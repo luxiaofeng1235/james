23748412f6ba579b77e56c364cb2e228d7fed84d
卢晓峰 <luxiaofeng.200@163.com>
2024-02-21 18:50:59 +0800

    吸怪

--- request/book_run.php
+++ request/book_run.php
@@ -18,9 +18,56 @@ require_once($dirname.'/library/init.inc.php');
 
 use QL\QueryList;##引入querylist的采集器
 
-$link_url =Env::get('APICONFIG.API_HOST');//域名地址
+$link_url =Env::get('APICONFIG.WEB_SOTRE_HOST');//域名地址
 $api_host = Env::get('APICONFIG.API_HOST'); //请求的接口域名
 $ql = QueryList::getInstance();
+$table_novel_name = 'ims_novel_info'; //小说明细表
+$table_chapter_name = 'ims_chapter';//章节表
+$sql = "select m.chapter_id,m.link_url,n.novelid from ".$table_novel_name.' as n left join '.$table_chapter_name.' as m on n.store_id = m.store_id where m.store_id =3 and m.chapter_id =621 limit 1';
+$items = $mysql_obj->fetchAll($sql ,'db_slave');
+
+
+function getAPIUrl($novelid,$url){
+    global $api_host,$link_url;
+    $urls = explode('/' , $url);
+    $store_name = $urls[1] ?? '';
+    $pager_data = end($urls);
+    $pager_data = str_replace('.html','',$pager_data);
+    list($chapterid,$page) = explode('_',$pager_data);
+    
+    //拼装Refer和urls的参数
+    $Referer = $link_url . $url;
+    //替换URL的相关信息
+    $api_url = $link_url .$api_host;
+    $api_url = str_replace('{$novelid}',$novelid, $api_url);
+    $api_url = str_replace('{$chapterid}',$chapterid , $api_url);
+    $api_url = str_replace('{$page}',$page , $api_url);
+    //组装header信息
+    $headers  = [
+        'headers' => [
+            'Referer'             =>    $Referer,
+            'Cache-Control'       =>    'Cache-Control',
+            'X-Requested-With'    =>    'XMLHttpRequest',
+
+        ]
+    ];
+
+    $items['headers'] = $headers;
+    $items['api_url'] = $api_url;
+    return $items;
+} 
+
+if($items && is_array($items)){
+    
+    foreach ($items as $val){
+        $url = getAPIUrl($val['novelid'],$val['link_url']);
+        echo '<pre>';
+        print_R($url);
+        echo '</pre>';
+        exit;
+    }
+}
+echo $sql;die;
 //////////////////////取小说的htm
 $url = 'https://www.souduw.com/api/novel/chapter/transcode.html?novelid=327782&chapterid=19&page=1';
 $headers  = [
