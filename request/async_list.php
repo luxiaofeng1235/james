546d8b970e1e165f18551c2e356fc2a5282ad865
卢晓峰 <luxiaofeng.200@163.com>
2024-02-22 13:04:51 +0800

    1

--- request/async_list.php
+++ request/async_list.php
@@ -15,10 +15,9 @@ if(!$countNum){
 
 $num =50;
 $pages = ceil($countNum /$num);
-$php_path = dirname(dirname(dirname(__DIR__))).'/Extensions/php/php7.2.9nts/php.exe';
+$php_path = dirname(dirname(dirname(__DIR__))).'/Extensions/php/php7.2.9nts/php.exe';//定义PHP扩展的路径
 $php_path = str_replace('\\','/',$php_path);
-
-$shell_cmd = $php_path . ' '.ROOT . '/request/aa.php';//需要执行的脚本
+$shell_cmd = $php_path . ' '.ROOT . '/request/aa.php';//需要执行的脚本的路径信息
 echo $shell_cmd."\n";
 
 
