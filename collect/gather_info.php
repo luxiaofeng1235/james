688423ce0bace06ffa2fb3864c2a19bc5584b0dc
卢晓峰 <luxiaofeng.200@163.com>
2024-02-23 11:10:25 +0800

    12

--- collect/gather_info.php
+++ collect/gather_info.php
@@ -28,9 +28,9 @@ if($info){
         'cover_logo'       =>array('#fmimg img','src'),//小说封面
         'author'    => array('#info p:eq(0)','text'),//小说作者
         'title'     =>array('#info>h1','text'),//小说标题
-        // 'status'    =>array('.jieshao .rt em:eq(1)','text'),//小说的状态
+        'status'    =>array('meta[property=og:novel:status]','content'),//小说的状态
         'third_update_time'    =>array('#info p:eq(2)','text'), //最近的更新时间
-        // 'nearby_chapter'    =>array('.jieshao .rt em:eq(3) a','text'), //最近的文章
+        'nearby_chapter'    =>array('meta[property=og:novel:latest_chapter_name]','content'), //最近的文章
         'intro' => array('#intro','text'),//小说的简介
         'location'  =>  array('.con_top','text'),//小说的面包屑位置
         // 'link_url'    =>array('.place a:eq(2)','href'),//当前书籍的url
@@ -40,8 +40,14 @@ if($info){
     $info_data=QueryList::get($story_link)
             ->rules($rules)
             ->query()->getData();
+    echo '<pre>';
+    print_R($info_data);
+    echo '</pre>';
+    exit;
     $store_data = $info_data->all();
     if(!empty($store_data)){
+
+        $story_id = trim($info[0]['story_id']); //小说的id
         //处理空字符串
         $location = str_replace("\r\n",'',$store_data['location']);
         $location =trim($location);
@@ -56,8 +62,11 @@ if($info){
         $author_data = explode('：',$store_data['author']);
         $store_data['author']  = $author_data[1] ?? '';
         $store_data['updatetime'] = time();
+        //执行更新操作
+        $where_data = "story_id = '".$story_id."'";
+        $update_ret = $mysql_obj->update_data($store_data,$where_data,$table_novel_name);
         echo '<pre>';
-        print_R($store_data);
+        print_R($update_ret);
         echo '</pre>';
         exit;
     }
