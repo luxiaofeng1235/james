ebeb7e142635a5c9a333de07320a16e09d232a3c
卢晓峰 <luxiaofeng.200@163.com>
2024-02-23 11:49:03 +0800

    111

--- collect/gather_info.php
+++ collect/gather_info.php
@@ -100,6 +100,12 @@ if($info){
         //处理相关的信息
         delete_chapter_data($update_id,$story_id,$chapter_table_name);
         $update_ret = $mysql_obj->update_data($store_data,$where_data,$table_novel_name);
+        $res = $mysql_obj->add_data($item_list , $chapter_table_name);
+        if($res){
+            //更新小说表的is_async为1，表示已经更新过了不需要重复更新
+            $update_data['is_async'] = 1;
+            $a= $mysql_obj->update_data($update_data,$where_data,$table_novel_name);
+        }
         echo "insert_id：".$update_id."\t当前小说：".$store_data['title']."|novelid=".$story_id." ---url：".$story_link."\t拉取成功，共更新章节目录：".count($item_list)."个\r\n";
     }
 }else{
