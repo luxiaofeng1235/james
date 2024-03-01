276cf9d44f0aa7ffc5ce6071f6a597785b0e12fa
卢晓峰 <luxiaofeng.200@163.com>
2024-03-01 15:01:22 +0800

    111

--- download_html.php
+++ download_html.php
@@ -5,28 +5,61 @@ $dirname =str_replace("\\", "/", $dirname) ;
 ini_set('memory_limit','9000M');
 require_once($dirname.'/library/init.inc.php');
 use QL\QueryList;
-$list = $mysql_obj->fetchAll("select * from ims_link_url limit 0,200",'db_slave');
-$urls = array_column($list,'story_link');
-$items = MultiHttp::curlGet($urls,null,true);
+set_time_limit(0);
 
-$html_data = [];
-$rules = array(
-    'text' => array('meta[og:novel:read_url]','content'),//采集class为two下面的超链接的链接
-);
-foreach($items as $key =>$val){
-    $data = QueryList::html($val)
-            ->rules($rules)->query()
-            ->getData();
-    $json_data = $data->all();
-    $html_data[$json_data['text']] =$val;
+$exec_start_time =microtime(true);
+
+$all_num = 10000;//先执行一万本，看下内存
+$limit = 5000;
+$t = $all_num/$limit;
+
+
+$curl_num = 200;
+for ($i=0; $i <$t ; $i++) {
+    $str = $i*$limit.','.$limit;
+    $list = $mysql_obj->fetchAll("select * from ims_link_url limit ".$str,'db_slave');
+    $urls = array_column($list,'story_link');
+    $item = array_chunk($urls,$curl_num);
+    foreach($item as $key =>$url){
+        curlGetHtml($url);
+        die;
+
+    }
 }
 
-$save_dir=  'E:\html_data';
-foreach($html_data as $key =>$val){
-    $prefix = str_replace('/','',$key);
-    $filename = $save_dir.DS .'info_'.$prefix.'.html';
-    file_put_contents($filename,$val);
+
+$exec_end_time =microtime(true); //执行结束时间
+$executionTime = $exec_end_time - $exec_start_time;
+echo "Script execution time: ".sprintf('%.2f',($executionTime/60))." minutes \r\n";
+echo "over\r\n";
+
+//采集内容到本地
+function curlGetHtml($urls){
+    if(!$urls) return false;
+    $rules = array(
+        'text' => array('meta[property=og:novel:read_url]','content'),//采集class为two下面的超链接的链接
+    );
+    $items = MultiHttp::curlGet($urls,null,true);
+    $html_data = [];
+    foreach($items as $key =>$val){
+            $data = QueryList::html($val)
+                ->rules($rules)->query()
+                ->getData();
+        // $prefix = str_replace('/','',$key);
+        $json_data = $data->all();
+        $html_data[$json_data['text']] =$val;
+    }
+    $save_dir=  'E:\html_data';
+    if(!empty($html_data)){
+        foreach($html_data as $k =>$v){
+             $index = str_replace('/','',$k);
+             $file_name = $save_dir . DS . 'detail_'.$index.'.html';
+             file_put_contents($file_name,$v);
+        }
+    }
 }
-// echo "over\r\n";
+
+
+
 
 ?>
