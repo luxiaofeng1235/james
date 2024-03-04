424b90a00952bee4b7fe7fe3281fb7345768422c
卢晓峰 <luxiaofeng.200@163.com>
2024-03-04 14:08:24 +0800

    同步处理本地的存储文件信息

--- paoshu8/local_file.php
+++ paoshu8/local_file.php
@@ -11,7 +11,7 @@ $dirname =str_replace("\\", "/", $dirname) ;
 require_once($dirname.'/library/init.inc.php');
 
 $id = isset($argv[1]) ? trim($argv[1]) : '';
-
+$novel_table_name = Env::get('APICONFIG.TABLE_NOVEL');
 if(!empty($id)){
     $run_data = explode(',',$id);
     //通过cli的方式来进行配置开启多个窗口抓取
@@ -31,38 +31,49 @@ use QL\QueryList;##引入querylist的采集器
 $exec_start_time = microtime(true); //执行开始时间
 $sql = "select story_id,story_link,pro_book_id from ims_novel_info where $where limit 3000";
 $list = $mysql_obj->fetchAll($sql,'db_slave');
+if($list){
+    $num = 200;//一次性抓取30个页面
+    foreach($list as $key =>$val){
+        $pro_book_id = intval($val['pro_book_id']); //线上的对应的小说id
+        $story_id = trim($val['story_id']);
+        $download_path =Env::get('SAVE_NOVEL_PATH') .DS . $pro_book_id;//下载路径
+        // echo $download_path;die;
+        // //创建地址目录信息
+        if(!is_dir($download_path)){
+            createFolders($download_path);
+        }
+        if(!$pro_book_id){
+            echo "未同步线上小说id\r\n";
+            continue;
+        }
+        $sql = "select novelid,link_name, link_url  from ims_chapter where story_id ='".$story_id."'";
+        $chapter_item = $mysql_obj->fetchAll($sql,'db_slave');
+        if(!$chapter_item) continue;
+        $items = array_chunk($chapter_item,$num);
+        $ids = [];
+        foreach($items as $k =>&$v){
+            //抓取内容信息
+            $html_data= getContenetNew($v);
+            $ids = array_column($v, 'novelid');
+            //更新对应的is_async状态
+            // //保存本地存储数据
+            saveLocalFile($download_path,$html_data);
+        }
 
-$num = 200;//一次性抓取30个页面
-foreach($list as $key =>$val){
-    $pro_book_id = intval($val['pro_book_id']); //线上的对应的小说id
-    $download_path =Env::get('SAVE_NOVEL_PATH') .DS . $pro_book_id;//下载路径
-    // echo $download_path;die;
-    // //创建地址目录信息
-    if(!is_dir($download_path)){
-        createFolders($download_path);
+        /***********更新小说里的章节已同步状态 start*****************/
+        $update_novel_data= ['syn_chapter_status'=>1];
+        $where_up_data = "story_id='".$story_id."' limit 1";
+        $mysql_obj->update_data($update_novel_data,$where_up_data,$novel_table_name);
+        /***********更新小说里的章节已同步状态 end******************/
+
+        echo "succes story_id：".$story_id."\t拉取本地章节：".count($chapter_item)."\turl:".$val['story_link']."\r\n";
     }
-    $story_id = trim($val['story_id']);
-    if(!$story_id){
-        continue;
-    }
-    $sql = "select novelid,link_name, link_url  from ims_chapter where story_id ='".$story_id."'";
-    $chapter_item = $mysql_obj->fetchAll($sql,'db_slave');
-    if(!$chapter_item) continue;
-    $items = array_chunk($chapter_item,$num);
-    $ids = [];
-    foreach($items as $k =>&$v){
-        //抓取内容信息
-        $html_data= getContenetNew($v);
-        $ids = array_column($v, 'novelid');
-        //更新对应的is_async状态
-        // //保存本地存储数据
-        saveLocalFile($download_path,$html_data);
-    }
-    echo "succes story_id：".$val['story_id']."\t拉取本地章节：".count($chapter_item)."\turl:".$val['story_link']."\r\n";
+    $exec_end_time =microtime(true); //执行结束时间
+    $executionTime = $exec_end_time - $exec_start_time;
+    echo "Script execution time: ".sprintf('%.2f',($executionTime/60))." minutes \r\n";
+}else{
+    echo "no data\r\n";
 }
-$exec_end_time =microtime(true); //执行结束时间
-$executionTime = $exec_end_time - $exec_start_time;
-echo "Script execution time: ".($executionTime/60)." minutes \r\n";
 
 /**
 * @note 生成保存的文件
