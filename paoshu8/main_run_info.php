e8ecbeafd6da49dee89a23dde3a488482d7a8a67
卢晓峰 <luxiaofeng.200@163.com>
2024-03-05 20:38:30 +0800

    1112

--- paoshu8/main_run_info.php
+++ paoshu8/main_run_info.php
@@ -10,18 +10,19 @@
 ini_set("memory_limit", "5000M");
 set_time_limit(0);
 require_once dirname(__DIR__).'/library/init.inc.php';
+//取出来所有未同步的数据信息
 $sql = "select store_id from ".Env::get('APICONFIG.TABLE_NOVEL')." where is_async = ".NovelModel::$is_no_async." limit 1";
 $list = $mysql_obj->fetchAll($sql,'db_slave');
 
 if($list){
+    //如果列表中存在可用数据信息
     foreach($list as $key =>$value){
         $store_id = intval($value['store_id']);
         if(!$store_id){
             continue;
         }
         $shell_cmd = 'nohup php gather_info_local.php '.$store_id.' &';
-        echo $shell_cmd;
-        die;
+        echo 'shell-cmd：'. $shell_cmd . PHP_EOL;
         exec($shell_cmd , $output,$status);
         echo '<pre>';
         print_R($shell_cmd);
