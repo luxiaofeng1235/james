f00ea12ab8476491af31f5eb490f6d77208fe484
卢晓峰 <luxiaofeng.200@163.com>
2024-03-09 15:42:33 +0800

    脚本批量额更新处理

---
+++ paoshu8/shell_cmd/run_daemon_40000_60000.sh
@@ -0,0 +1,33 @@
+#bin/bash
+i=10
+host="127.0.01"
+port="3306"
+user="root"
+password="HM9GO3JH3XrLoouh"
+database="book_center"
+
+query="SELECT store_id,story_id,pro_book_id,title FROM ims_novel_info where is_async = 0 and store_id>40000 and store_id<60000 limit 1"
+#echo "$query"
+result=$(mysql -h $host -u $user -p$password $database -s -e "$query")
+
+
+if [ $? -ne 0 ]; then
+    echo "数据库连接失败或查询错误：$result"
+    exit 1
+fi
+#echo "$result"
+
+#echo "$shell_cmd"
+
+NAME=VALUEBANK
+#eval $shell_cmd
+while IFS=$'\t' read -r column1 column2 column3 column4; do
+    # 在这里处理每一行数据，可以使用变量column1、column2、column3
+    #   # 例如打印每一行的数据
+    echo "store_id: $column1"
+    shell_cmd="cd /www/wwwroot/work_project/novelProject/paoshu8/ && nohup /www/server/php/72/bin/php gather_info_local.php $column1 >> run_store.out 2>&1 &"
+    echo $shell_cmd | bash;
+    #echo "nohup php aa.php  $column1  >>tst.out  2>&1" | bash;
+done <<< "$result"
+
+
