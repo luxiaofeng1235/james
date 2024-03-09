f70fae8ca38e211741420ce52c73e7d380b312c1
卢晓峰 <luxiaofeng.200@163.com>
2024-03-09 14:23:38 +0800

    修改定时脚本更新

--- paoshu8/run.sh
+++ paoshu8/run.sh
@@ -1,20 +1,33 @@
 #bin/bash
-
 i=10
+host="127.0.01"
+port="3306"
+user="root"
+password="HM9GO3JH3XrLoouh"
+database="book_center"
 
-MYSQL_HOST='localhost'
-MYSQL_USER='root'
-MYSQL_PASS='root'
-MYSQL_DATABASE='book_center'
-MYSQL_PORT='3306'
+query="SELECT store_id,story_id,pro_book_id,title FROM ims_novel_info where is_async = 0  limit 1"
+#echo "$query"
+result=$(mysql -h $host -u $user -p$password $database -s -e "$query")
 
-#定义查询语句
-SQL_QUERY1='select * from ims_novel_info limit 3;'
 
-# use $MYSQL_DATABASE;
-mysql -h localhost -uroot -proot -D book_center
-echo 3
-#mysql -h $MYSQL_HOST -P $MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASS assurance_acct -A --default-character-set=utf8 -N -e "
-#SELECT * FROM ims_novel_info limit 3;
-#"
+if [ $? -ne 0 ]; then
+    echo "数据库连接失败或查询错误：$result"
+    exit 1
+fi
+#echo "$result"
+
+#echo "$shell_cmd"
+
+NAME=VALUEBANK
+#eval $shell_cmd
+while IFS=$'\t' read -r column1 column2 column3 column4; do
+    # 在这里处理每一行数据，可以使用变量column1、column2、column3
+    #   # 例如打印每一行的数据
+    echo "store_id: $column1"
+    shell_cmd="cd /www/wwwroot/work_project/novelProject/paoshu8/ && nohup /www/server/php/72/bin/php gather_info_local.php $column1 >> run_store.out 2>&1 &"
+    echo $shell_cmd
+    #echo "nohup php aa.php  $column1  >>tst.out  2>&1" | bash;
+done <<< "$result"
+
 
