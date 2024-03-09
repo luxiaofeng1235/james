80a4bbae82e0be7b4d81ea38ce7cdf8f1eb30dc9
卢晓峰 <luxiaofeng.200@163.com>
2024-03-09 22:13:25 +0800

    1112

--- paoshu8/shell_cmd/run_daemon_180000_200000.sh
+++ paoshu8/shell_cmd/run_daemon_180000_200000.sh
@@ -15,19 +15,18 @@ if [ $? -ne 0 ]; then
     echo "数据库连接失败或查询错误：$result"
     exit 1
 fi
-#echo "$result"
 
-#echo "$shell_cmd"
-
-NAME=VALUEBANK
-#eval $shell_cmd
-while IFS=$'\t' read -r column1 column2 column3 column4; do
-    # 在这里处理每一行数据，可以使用变量column1、column2、column3
-    #   # 例如打印每一行的数据
-    echo "store_id: $column1"
-    shell_cmd="cd /www/wwwroot/work_project/novelProject/paoshu8/ && nohup /www/server/php/72/bin/php gather_info_local.php $column1 >> run_store.out 2>&1 &"
-    echo $shell_cmd | bash;
-    #echo "nohup php aa.php  $column1  >>tst.out  2>&1" | bash;
-done <<< "$result"
-
+echo "$result"
+if [ ! -n "$result" ]; then
+    echo "DATA IS NULL"
+else
+    NAME=VALUEBANK
+    while IFS=$'\t' read -r column1 column2 column3 column4; do
+        # 在这里处理每一行数据，可以使用变量column1、column2、column3
+        #   # 例如打印每一行的数据
+        echo "store_id: $column1"
+        shell_cmd="cd /www/wwwroot/work_project/novelProject/paoshu8/ && nohup /www/server/php/72/bin/php gather_info_local.php $column1 >> run_store.out 2>&1 &"
+        echo $shell_cmd | bash;
+    done <<< "$result"
+fi
 
