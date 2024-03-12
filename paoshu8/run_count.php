61f84d77e4c79e7af5ad3a800c7d5a3b116ea8dc
卢晓峰 <luxiaofeng.200@163.com>
2024-03-12 16:05:45 +0800

    QQ

--- paoshu8/run_count.php
+++ paoshu8/run_count.php
@@ -13,13 +13,16 @@ $between_where = [
     [140000,160000],[160000,180000],[180000,200000],[200000,300000]
 ];
 $is_async = 0;//未同步的数据
+$run_time_count = 0;
 foreach($between_where as $key =>$val){
     if(!$val || !isset($val[0]) || !isset($val[1]))
         continue;
     $sql = "SELECT count(store_id) as num FROM ".Env::get('APICONFIG.TABLE_NOVEL')." where is_async =".$is_async. " and store_id > ".$val[0]." and store_id < ".$val[1];
     $info  = $mysql_obj->fetch($sql , 'db_slave');
     $ts_count = $info[0] ?? 0;
+    $run_time_count+=$ts_count;//总数
     echo $val[0].'-'.$val[1]." nend to run nums：".$ts_count.PHP_EOL;
 }
+echo "all-no-run nums：".$run_time_count."\r\n";
 echo "finish\r\n";
 ?>
