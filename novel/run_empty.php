e6aaa83308151071d5f5aa9035b11aca24a36738
卢晓峰 <luxiaofeng.200@163.com>
2024-03-19 14:31:28 +0800

    处理

--- novel/run_empty.php
+++ novel/run_empty.php
@@ -32,7 +32,6 @@ $info =$mysql_obj->fetch($sql , 'db_slave');
 if(!empty($info)){
     extract($info);
     //同步一下图片，直接取
-    $t= NovelModel::saveImgToLocal($cover_logo , $title , $author);
     $md5_str= NovelModel::getAuthorFoleder($title,$author);
     echo "book_md5：".$md5_str."\r\n";
     $json_path = Env::get('SAVE_JSON_PATH').DS.$md5_str.'.'.NovelModel::$json_file_type;
@@ -60,6 +59,8 @@ if(!empty($info)){
         $list = NovelModel::removeAdInfo($arr);
         return $list;
     };
+
+
     //处理广告并移除关联章节
     $chapter_list = $removeAdInfo($chapter_list);
     foreach($chapter_list as $val){
@@ -84,22 +85,41 @@ if(!empty($info)){
     }
     //统计下当前的跑出来的数据情况
     echo "json-list-num：".count($chapter_list)."\tis_have_num：".$i."\tall-empty-num：".count($dataList).PHP_EOL;
-    $tmp_size = 1; //每次定义20个步长去处理
-    $items = array_slice($dataList , 0, $tmp_size); //测试后期删掉
-    echo "共需要处理的空章节总数--步长按照{$tmp_size}来算的:".count($items).PHP_EOL;
+    // $tmp_size = 10; //每次定义20个步长去处理
+    // $items = array_slice($dataList , 0, $tmp_size); //测试后期删掉
+    // echo "共需要处理的空章节总数--步长按照{$tmp_size}来算的:".count($items).PHP_EOL;
     echo "-----------------------------\r\n";
+    $curlMulti = new curl_pic_multi();
     $limit_num = Env::get('LIMIT_EMPTY_SIZE');
-    $items = array_chunk($items, $limit_num);
-    $goodsList = [];
-    foreach($items as $k =>$v){
-        //抓取远端地址并进行处理
-        $curl_contents= NovelModel::getHtmlData($v);
-        if(!$curl_contents)
-            continue;
-        //保存本地文件变更
-        NovelModel::saveLocalContent($curl_contents);
-        sleep(1);
-    }
+    $items = array_chunk($dataList, $limit_num);
+    $all_count = count($dataList);
+    $save_list = [];
+    //轮训处理获取关联的数据信息
+    do{
+        $success_num= 0;
+        $insert_data = [];
+        foreach($items as $k =>$v){
+            //抓取远端地址并进行处理
+            $curl_contents= NovelModel::getHtmlData($v);
+            if(!$curl_contents)
+                continue;
+             if($curl_contents && is_array($curl_contents)){
+                foreach($curl_contents as $gval){
+                    if(!empty($gval['content'])){
+                        $success_num++;
+                        $insert_data[]=$gval;
+                    }
+                }
+            }
+        }
+        //判断如果是当前请求和连接一直就退出
+        if($all_count == $success_num){
+            $save_list = $insert_data;
+            break;
+        }
+    }while(true);
+    //保存对应的文章信息
+    NovelModel::saveLocalContent($save_list);
     echo "store_id = ".$info['store_id']." | pro_book_id = ".$info['pro_book_id'].PHP_EOL;
     echo "over\r\n";
 }else{
@@ -109,5 +129,16 @@ $exec_end_time = microtime(true);
 $executionTime = $exec_end_time - $exec_start_time;
 echo "Script execution time: ".round(($executionTime/60),2)." minutes \r\n";
 
-
+//获取成功抓取的总次数
+function getSuccessTimes($data){
+    if(!$data)
+        return false;
+    $num = 0;
+    foreach($data as $value){
+        if(!empty($value['content'])){
+            $num++;
+        }
+    }
+    return $num;
+}
 
