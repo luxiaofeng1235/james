60647fa0f6ecabcb69ab1ff423c707053a9f0272
卢晓峰 <luxiaofeng.200@163.com>
2024-03-24 01:31:15 +0800

    缓存处理

--- paoshu8/set_proxy_cache.php
+++ paoshu8/set_proxy_cache.php
@@ -8,31 +8,97 @@
 // 创建时间:下午7:12:00
 // 编 码：UTF-8
 // 摘 要:定时扫描redis里的代理是否存在，如果不存在就取取一个最新的可用的出来，方便业务使用
+// 只判断代理IP是否在缓存中，缓存30分钟，过期就重新运行
 $dirname = dirname(dirname(__FILE__));
 $dirname =str_replace("\\", "/", $dirname) ;
 ini_set('memory_limit','9000M');
 require_once($dirname.'/library/init.inc.php');
 $exec_start_time = microtime(true);
+
+
+
+// /**
+// * @note 获取并排除指定地区的代理IP
+// *
+// * @return array
+// */
+// function getAllowProxy(){
+//     global $url;
+//     //不适用的地区的IP
+//     $area_prxy =Env::get('NO_USE_PROXY_AREA');
+//     $no_area_proxy = explode(',',$area_prxy); //按照地区切割
+//     $no_area_proxy = array_unique($no_area_proxy);//去重
+//     $new_proxy = [];
+//     do{
+//         $flag = 0; //设置一个标记
+//         foreach($no_area_proxy as $ip_area){
+//             $info = webRequest($url,'GET');
+//             $proxy_info  =    json_decode($info , true);
+//             $proxy_data = $proxy_info['data'][0] ?? [];
+//             //判断如果不在这个地区内的，就跳出来用当前的IP
+//             if(isset($proxy_data['city']) &&  !strstr($proxy_data['city'] , $ip_area)){
+//                  $new_proxy = $proxy_info;
+//                  $flag = 1; //如果找到, 标记flag=1就跳出来
+//                  break;
+//             }
+//             sleep(1); //停止1秒
+//         }
+//         if( $flag!=0 ){
+//             break;
+//         }
+//     }while(true);
+//     $proxy_ret   = $new_proxy['data'][0] ?? []; //获取新的代理
+// }
+//允许命令行接收的参数
+$allowKey = [
+    'zhima_proxy_new:',
+    'zhima_mobile_key:',
+    'zhima_mobile_empty_key:',
+    'zhima_proxy_story_info:',
+];
+
+//从客户端的cli接收参数
+$redis_cache_key = isset($argv[1])  ? trim($argv[1]) : '' ;//接受的缓存的key
+if(!$redis_cache_key){
+    exit("请输入缓存的key");
+}else{
+    if(!in_array($redis_cache_key,$allowKey)){
+        exit("该缓存 ".$redis_cache_key." 无效，请输入有效的key ".var_export($allowKey,true));
+    }
+}
+
+$pro_type = isset($argv[3]) ? $argv[3] : 1;
+if($pro_type == 1){
+    //直连IP
+    $url = Env::get('ZHIMAURL');
+}else if($pro_type ==2){
+    //隧道IP
+    $url = Env::get('ZHIMA_SUIDAO');
+}
 $proxy_conf = [];
-$redis_cache_key = Env::get('ZHIMA_REDIS_KEY');
+echo "cache_key：".$redis_cache_key."\r\n";
+$host  =  isset($argv[2]) ? trim($argv[2]) : Env::get('APICONFIG.PAOSHU_HOST') ;
+echo "检测的url：".$host."\r\n";
+// $redis_data->del_redis($redis_cache_key);
 $proxy = $redis_data->get_redis($redis_cache_key);
 if(!$proxy){
     do{
-    $url = Env::get('ZHIMAURL');//获取配置的代理URL
-    $info = webRequest($url,'GET');
-    $proxy_info  =    json_decode($info , true);
-    $proxy_data = $proxy_info['data'][0] ?? [];
-     //检测目标网站是否可用，如果不可用就取一个可用的
-     $check_data = curlProxyState(Env::get('APICONFIG.PAOSHU_HOST'),$proxy_data);
-     if($check_data['http_code'] == 200){
-        //如果检测到可用的IP，就直接退出循环
-        $proxy_conf = $proxy_data;
-        break;
-     }
+        //获取排除指定地区的IP
+        $info = webRequest($url,'GET');
+        $proxy_info  =    json_decode($info , true);
+        $proxy_data = $proxy_info['data'][0] ?? [];
+        //检测目标网站是否可用，如果不可用就取一个可用的
+        $check_data = curlProxyState($host,$proxy_data);
+        if($check_data['http_code'] == 200){
+            //如果检测到可用的IP，就直接退出循环
+            $proxy_conf = $proxy_data;
+            break;
+        }
     }while(true);
-    $diff_time = 30 * 60;//默认先控制30分钟的缓存，防止提前过期
+    // $diff_time = 25 * 60;//默认先控制25分钟的缓存，防止提前过期
+    $diff_time = strtotime($proxy_data['expire_time']) - time(); //利用过期时间-当前时间为缓存的redis的时间
     $redis_data->set_redis($redis_cache_key,json_encode($proxy_conf),$diff_time);
-    echo "获取到新的代理信息\r\n";
+    echo "获取到新的代理IP信息（芝麻代理）\r\n";
     echo '<pre>';
     print_R($proxy_conf);
     echo '</pre>';
@@ -40,6 +106,8 @@ if(!$proxy){
 }else{
     echo "redis里的代理暂未过期,还能用\r\n";
     $proxy_conf = json_decode($proxy , true);
+    $ttl =$redis_data->ttl($redis_cache_key); //获取缓存的可用时间
+    echo "剩余可用缓存时间：".sprintf('%.2f',($ttl/60))." minutes\r\n";
     echo '<pre>';
     var_dump($proxy_conf);
     echo '</pre>';
