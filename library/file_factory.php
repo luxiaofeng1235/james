61bab1965582e9a36edc190cb26ea7023a6c01ba
卢晓峰 <luxiaofeng.200@163.com>
2024-03-23 23:19:11 +0800

    112

--- library/file_factory.php
+++ library/file_factory.php
@@ -1,13 +1,13 @@
 <?php
 // ///////////////////////////////////////////////////
 // Copyright(c) 2013,真一网络
-// 日 期：2024-3-6
+// 日 期：2024-03-06
 // 作　者：卢晓峰
 // E-mail :luxiaofeng@linktone.com
 // 文件名：file_factory.php
 // 创建时间:下午2:01:55
 // 编 码：UTF-8
-// 摘 要: 文件工厂类，自动实现文件和对象的定义
+// 摘 要: 文件工厂类，自动实现远端章节同步到本地
 // ///////////////////////////////////////////////////
 
 class FileFactory{
@@ -28,20 +28,39 @@ class FileFactory{
         $this->where_data .= $this->syn_wait_status;//搜索的前置条件
     }
 
-    public function testarr(){
-        return $this->num;
+     /**
+     * @note  更新状态
+     * @param string $story_id 小说ID
+     * @return string
+     */
+    function updateStatusInfo($store_id= 0){
+        if(!$store_id){
+            return false;
+        }
+        global $mysql_obj;
+        $where_condition = "store_id = '".$store_id."'";
+        $no_chapter_data['syn_chapter_status'] = 1;
+        $no_chapter_data['is_async'] = 1;
+        //对比新旧数据返回最新的更新
+        $mysql_obj->update_data($no_chapter_data,$where_condition,$this->table_novel_name);
     }
 
 
      /**
      * @note  同步章节信息
      * @param string $story_id 小说ID
+     * @param $info_data 小说基础信息
      * @return string
      */
     public function synChapterInfo($story_id = '',$info_data= []){
         if(!$story_id){
             return false;
         }
+        //校验代理IP是否过期
+        if(!NovelModel::checkProxyExpire()){
+            exit("代理IP已过期，请重新拉取最新的ip\r\n");
+        }
+        //判断数据是否为空
         if(!empty($info_data)){
             $info = $info_data;
             //判断当前小说是否已经同步
@@ -54,70 +73,130 @@ class FileFactory{
             $sql = "select story_id,story_link,pro_book_id,title from ims_novel_info where $where";
             $info = $this->mysql_conf->fetch($sql,'db_slave');
         }
-
-
         if(!empty($info)){
             $pro_book_id = intval($info['pro_book_id']); //线上的对应的小说id
-            $story_id = trim($info['story_id']);
+            $story_id = trim($info['story_id']); //小说网站id
+            $store_id = intval($info['store_id']); //库里的id
 
             //获取对应的加密串作为文件名：书名+作者
             $md5_str= NovelModel::getAuthorFoleder($info['title'] ,$info['author']);
 
             $download_path =Env::get('SAVE_NOVEL_PATH') .DS . $md5_str;//下载路径;
             if(!$pro_book_id){
+                $this->updateStatusInfo($store_id);
                 printlog('暂未同步线上pro_bok_id');
                 return false;
             }
             if(!is_dir($download_path)){
                 createFolders($download_path);
             }
-
-            //删除已经存在的文件，保证都是最新的
-            $base_dir =Env::get('SAVE_NOVEL_PATH') .DS .$md5_str;
-            $q  = (count(glob("$base_dir/*")) === 0) ?  0 : 1;
-            if($q){
-                $delete_file = $base_dir.'/*';
-                exec('rm -rf '.$delete_file,$output,$status);
-            }
-
             //记录文件的格式
             $file_name =Env::get('SAVE_JSON_PATH') .DS .$md5_str.'.' .NovelModel::$json_file_type;
             $json_data = readFileData($file_name);
             if(!$json_data) {
+                $this->updateStatusInfo($store_id);
+                echo "当前小说未生成json文件\r\n";
                 printlog('当前ID:'.$pro_book_id.'暂未生成json文件');
                 return false;
             }
             $chapter_item = json_decode($json_data,true);
             if(!$chapter_item)
                 return false;
-            //转换数据字典用业务里的字段，不和字典里的冲突
-            $chapter_item = NovelModel::changeChapterInfo($chapter_item);
+
+             //构造函数处理广告
+            $removeAdInfo = function($arr){
+                foreach($arr as &$val){
+                    $val['link_name'] = $val['chapter_name'];
+                }
+                //移除广告章节
+                $list = NovelModel::removeAdInfo($arr);
+                return $list;
+            };
+             //处理广告并移除关联章节
+            $chapter_item  = $removeAdInfo($chapter_item);
+            if(!$chapter_item){
+                $this->updateStatusInfo($store_id);
+                echo  "去除广告暂无发现需同步的章节了 \r\n";
+                die;
+            }
+
+            echo "总章节总数：".count($chapter_item).PHP_EOL;
+            $dataList = [];
+            foreach($chapter_item as &$val){
+                $filename =$download_path .DS . md5($val['chapter_name']).'.'.NovelModel::$file_type;
+                $content = readFileData($filename);
+                if(!$content ||$content =='从远端拉取内容失败，有可能是对方服务器响应超时，后续待更新'  || !file_exists($filename)){
+                    $val['link_url'] = $val['chapter_link'];
+                    $dataList[] =   $val;
+                }
+             }
+
+
+             if(!$dataList){
+                $this->updateStatusInfo($store_id);
+                exit("*********************************已经爬取完毕 ，不需要重复操作了\r\n");
+             }
+             echo "\r\n\r\n";
+             echo "共需要补的章节总数量： num = ".count($dataList)."\r\n";
+             //转换数据字典用业务里的字段，不和字典里的冲突
+            $dataList = NovelModel::changeChapterInfo($dataList);
+            ///测试--------------------------
+            $chapter_item = array_slice($chapter_item, 0,30);
+            // $curlMulti = new curl_pic_multi();
+
             //按照长度进行切割轮询处理数据
-            $items = array_chunk($chapter_item,$this->num);
+            // $items = array_chunk($chapter_item,$this->num);
+            $items = array_chunk($dataList,200);
+            $i_num = 0;
             foreach($items as $k =>&$v){
                 //抓取内容信息
-                $html_data= getContenetNew($v);
+                $html_data= getStoryCotents($v,$store_id,$md5_str);
+                // echo '<pre>';
+                // print_R($html_data);
+                // echo '</pre>';
+                // exit;
+                if($html_data){
+
+                    $a_num =0;
+                    foreach ($html_data as  $gvalue) {
+                        $a_num++;
+                        if(!empty($gvalue['content'])){
+                            echo "num：{$a_num} \t chapter_name: {$gvalue['chapter_name']}\t path：{$gvalue['save_path']} \r\n";
+                            $i_num++;
+                        }else{
+                             echo "num：{$a_num} \t chapter_name: {$gvalue['chapter_name']} \t 小说源内容为空 url：{$gvalue['chapter_link']}\r\n";
+                        }
+                    }
+                }
                 //保存本地存储数据
                 $this->synLocalFile($download_path,$html_data);
-                sleep(3);//休息三秒不要立马去请求，防止空数据的发生
+                sleep(1);//休息三秒不要立马去请求，防止空数据的发生
             }
+            echo "共抓取下来的章节总数：".$i_num.PHP_EOL;
             unset($items);
             //强制清除内存垃圾
             gc_collect_cycles();
             unset($items);
             unset($chapter_item);
             //更细对应的状态信息
-            //更新对应的is_async状态
-            $update_novel_data= [
-                'syn_chapter_status'=> $this->syn_success_status,
-            ];
-            $where_up_data = "story_id='".$story_id."' limit 1";
-            $this->mysql_conf->update_data($update_novel_data,$where_up_data,$this->table_novel_name);
+            //更新对应的is_async和syn_success_status状态
+            $this->updateStatusInfo($store_id);
             printlog('小说（'.$info['title'].'）|pro_book_id='.$pro_book_id.'|story_id='.$story_id.'同步章节完成');
             return true;
         }else{
             printlog('story_id = '.$story_id.'未匹配数据');
         }
+    }
+
+     /**
+     * @note  转换添加的小说信息
+     * @param string $datas  小说数据
+     * @return string
+     */
+    protected function exchangeNovelData($datas= []){
+        if(!$datas)
+            return false;
+        return $datas;
     }
 
      /**
@@ -128,19 +207,13 @@ class FileFactory{
      */
     protected function synLocalFile($save_path,$data){
         foreach($data as $key =>$val){
-            if( !$val ) continue;
             $content = $val['content'] ?? '';//提交的内容
-            //这样子保证每个里面都有内容
-            // if(!$content || empty($content)){
-            //     $content ='从远端拉取内容失败，有可能是对方服务器响应超时，后续待更新';
-            // }
-            //只有有内容才去创建内容
-            if($content){
-                //用md5加密的方式去更新
-                $filename = $save_path .DS. md5($val['link_name']).'.'.NovelModel::$file_type;
-                file_put_contents($filename,$content); //防止文件名出错
-            }
-
+            $save_path = $val['save_path'] ?? '';
+            if(!$save_path || !$content) continue;
+            writeFileCombine($save_path, $content);
+             //用md5加密的方式去更新
+            // $filename = $save_path .DS. md5($val['link_name']).'.'.NovelModel::$file_type;
+            // file_put_contents($filename,$content); //防止文件名出错
         }
     }
 }
