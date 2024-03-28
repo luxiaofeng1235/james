4cd65591f22dec3e91f957a177f33219dbd2e100
卢晓峰 <luxiaofeng.200@163.com>
2024-03-28 21:46:07 +0800

    优化

--- paoshu8/main_run_info.php
+++ paoshu8/main_run_info.php
@@ -11,31 +11,20 @@ ini_set("memory_limit", "5000M");
 set_time_limit(0);
 require_once dirname(__DIR__).'/library/init.inc.php';
 //取出来所有未同步的数据信息
-$where =  "is_async = ".NovelModel::$is_no_async;
-// $where .=" and story_id ='95_95678'";
 
-$sql = "select store_id from ".Env::get('APICONFIG.TABLE_NOVEL')." where $where limit 1";
-// echo $sql;die;
-$list = $mysql_obj->fetchAll($sql,'db_slave');
-if($list){
-    //如果列表中存在可用数据信息
-    foreach($list as $key =>$value){
-        $store_id = intval($value['store_id']);
-        // $pro_book_id = intval($value['pro_book_id']);
-        if(!$store_id){
-            continue;
-        }
-        //挂起在后台去运行
-        $shell_cmd = 'cd '.NovelModel::cmdRunPath().' && nohup '.Env::get('PHP_BIN_PATH').' gather_info_local.php '.$store_id.'  > '.Env::get('NOHUP_BIN_PATH').' 2>&1 &';
-        echo 'shell-cmd：'. $shell_cmd . PHP_EOL;
-        die;
-        exec($shell_cmd , $output,$status);
-        echo '<pre>';
-        print_R($shell_cmd);
-        echo '</pre>';
-        echo PHP_EOL;
-        echo " run store_id：".$store_id."\r\n";
-    }
+$list =file('./1000_3000_txt.txt');
+foreach($list as &$v){
+    $v = str_replace("\r\n",'',$v);
 }
-echo "over\r\n";
+
+$sql = "select store_id from ims_novel_info where is_async= 0 and  pro_book_id in (".implode(',',$list).")";
+$item = $mysql_obj->fetchAll($sql,'db_slave');
+
+
+$ids = implode(',',array_column($item, 'store_id'));
+
+
+$sql = "select store_id from ims_novel_info where is_async = 0 and store_id in (".$ids.")";
+echo $sql;
+
 ?>
