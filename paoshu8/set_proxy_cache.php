245208d00467a8393a94abc916e75949d131f0ba
卢晓峰 <luxiaofeng.200@163.com>
2024-04-08 11:49:59 +0800

    112

--- paoshu8/set_proxy_cache.php
+++ paoshu8/set_proxy_cache.php
@@ -118,7 +118,7 @@ if(!$proxy){
     $ttl =$redis_data->ttl($redis_cache_key); //获取缓存的可用时间
     $minutes = sprintf('%.2f',($ttl/60)); //剩余的分钟数
     if($minutes <= 10){
-        echo "当前缓存小于{$minutes}分钟 ，需要清理掉缓存：{$redis_cache_key}，重新获取\r\n";
+        echo "为了保证爬虫程序的可用性，提升效率，当前缓存小于{$minutes}分钟 ，需要清理掉缓存：{$redis_cache_key}，重新获取\r\n";
         $redis_data->del_redis($redis_cache_key);
     }
     echo "剩余可用缓存时间：".$minutes." minutes\r\n";
