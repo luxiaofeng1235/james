18266ba30098077985071ff6fab277bfd0a0c638
卢晓峰 <luxiaofeng.200@163.com>
2024-04-15 20:09:18 +0800

    112

--- yurunhttp.php
+++ yurunhttp.php
@@ -21,7 +21,7 @@ ConnectionPool::enable();
 
 // 为这个地址设置限制连接池连接数量3个
 // 一定不要有 / 及后续参数等
-ConnectionPool::setConfig('http://www.baidu.com', 10);
+ConnectionPool::setConfig('https://www.xsw.tw', 10);
 
 Co\run(function () {
     dumpPoolInfo();
@@ -31,13 +31,13 @@ Co\run(function () {
     });
 
     $wg = new \Swoole\Coroutine\WaitGroup();
-    for ($i = 0; $i < 200; ++$i)
+    for ($i = 0; $i < 20; ++$i)
     {
         $wg->add();
         go(function () use ($wg,$i) {
             $http = new HttpRequest();
-            $response = $http->get('http://www.baidu.com/?keywords=112');
-            var_dump($response->body());
+            $response = $http->get('https://www.xsw.tw/book/1654104.html');
+            // var_dump($response->body());
 
             echo  "num ={$i} +++++++++++++++++++++++++++++++++++++++ \r\n";
             $wg->done();
