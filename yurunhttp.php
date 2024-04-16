3c65a053e1588f6be3548171de44a4ff1571cb86
卢晓峰 <luxiaofeng.200@163.com>
2024-04-16 18:32:21 +0800

    112

--- yurunhttp.php
+++ yurunhttp.php
@@ -37,6 +37,7 @@ Co\run(function () {
     {
         $wg->add();
         go(function () use ($wg,$i) {
+            co::sleep(1);
             $http = new HttpRequest();
             $response = $http->get('https://www.baidu.com/s?wd=%E5%AD%A6%E4%B9%A0%E6%80%BB%E4%B9%A6%E8%AE%B0%E5%BC%BA%E8%B0%83%E7%9A%84%E6%80%BB%E4%BD%93%E5%9B%BD%E5%AE%B6%E5%AE%89%E5%85%A8%E8%A7%82&sa=fyb_n_homepage&rsv_dl=fyb_n_homepage&from=super&cl=3&tn=baidutop10&fr=top1000&rsv_idx=2&hisfilter=1');
             var_dump(strlen($response->body()),$response->getStatusCode());
