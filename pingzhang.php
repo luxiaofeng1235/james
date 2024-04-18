c7c6951709c36decce99c0765fc101a877248264
卢晓峰 <luxiaofeng.200@163.com>
2024-04-18 18:55:41 +0800

    112

--- pingzhang.php
+++ pingzhang.php
@@ -9,22 +9,29 @@ use Swoole\Coroutine;
 use Yurun\Util\HttpRequest;
 
 
-function getUrl(){
+function getUrl($urls = []){
+    if(!$urls){
+        return false;
+    }
+    $urls = array_filter($urls); //防止有空的url存在
     $items = [];
     $exec_start_time = microtime(true);
-    run(function () use(&$items){
+    run(function () use(&$items,$urls){
         $barrier = Barrier::make();
         $count = 0;
-        $N = 10;
-        foreach (range(1, $N) as $i) {
-            Coroutine::create(function () use ($barrier, &$count,$i,&$items) {
+        $N = count($urls);
+        $t_url =$urls;
+        echo "urls num：".$N."\r\n";
+        foreach (range(0, $N-1) as $i) {
+            $link = $urls[$i];
+            Coroutine::create(function () use ($barrier, &$count,$i,&$items,$urls) {
                 $http = new HttpRequest;
                  $response = $http->ua('YurunHttp')
                                  // ->proxy('7b2f9a6713186a90.asd.as.roxlabs.vip', '4600', 'socks5') //认证类型设置
                                  // ->proxyAuth('user-red1235-region-tw-sessid-twZh0b2ezH-sesstime-5-keep-true','123456abc') //认证账密
-                                 ->get('https://www.xsw.tw/book/1144673/248318027.html');
+                                 ->get('http://www.baidu.com');
                 // echo $file.PHP_EOL;
-                echo "num = {$i} \r\n";
+                echo "num = {$i} \t url = {$urls[$i]}\r\n";
                 $items[]=$response->body();
                 var_dump(strlen($response->body()),$response->getStatusCode());
                 // if($response->getStatusCode() != 200){
@@ -40,7 +47,14 @@ function getUrl(){
     return $items;
 }
 
-$list = getUrl();
+$url  = 'https://www.xsw.tw/book/1144673/248318027.html';
+for ($i=0; $i <40 ; $i++) {
+    $urls [] = $url;
+}
+
+$list = getUrl($urls);
+echo 333;exit;
+
 echo '<pre>';
 print_R($list);
 echo '</pre>';
