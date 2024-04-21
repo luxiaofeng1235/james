94d6fa2aae8d0505531935c5b77b9dd9a222a636
卢晓峰 <luxiaofeng.200@163.com>
2024-04-21 20:10:06 +0800

    11122

--- xsw/deal_page.php
+++ xsw/deal_page.php
@@ -31,17 +31,21 @@ $size  = explode(',',$limit);
 $pages = range($size[0] , $size[1]);
 
 
-$pages = array_slice($pages,0 , 1);
+$pages = array_slice($pages,0 , 10);
 
 $dataList =$urls= [];
 //生成页面链接方便进行爬取
 foreach($pages as $page){
    //替换相关的关联参数信息
     $url = StoreModel::replaceParam(Env::get('TWCONFIG.API_HOST_COMPLATE'),'pages',$page);
-    $urls[]= $url;
+    $dataList[]= [
+        'story_link'    =>  $url,
+    ];
 }
+$urls = array_column($dataList,'story_link');
 //设置配置细腻
 $item = StoreModel::swooleRquest($urls);
+
 if(!empty($item)){
     $num = 0;
     foreach($item as $key => $val){
@@ -49,7 +53,7 @@ if(!empty($item)){
         $page = $pages[$key+1] ?? 1;
         //保存的文件名生成规则
         $save_file = $download_path.DS. StoreModel::$page_name.$page .'.'.StoreModel::$file_type;
-        echo "num = $num \t url = {$urls[$key]}  \t path = {$save_file} \t complate \r\n";
+        echo "No. = $num \t url = {$urls[$key]}  \t path = {$save_file} \t complate \r\n";
     }
 }
 echo "finish\r\n";
