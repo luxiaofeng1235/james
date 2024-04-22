1b069696f60c3dd62fff4d421c7a55298fcb0a61
卢晓峰 <luxiaofeng.200@163.com>
2024-04-22 10:34:11 +0800

    112

--- paoshu8/refresh_pic_existis.php
+++ paoshu8/refresh_pic_existis.php
@@ -19,49 +19,52 @@ $exec_start_time = microtime(true);
 $store_id = isset($argv[1])   ? $argv[1] : 0; //匹配对应的ID去关联
 $db_name = 'db_novel_pro';
 $redis_key = 'img_pic_id';//redis的对应可以设置
-// $redis_data->set_redis($redis_key,500);
+// echo $redis_data->set_redis($redis_key,200000);
+// echo 333;exit;
 $id = $redis_data->get_redis($redis_key);
 
-$where_data = '  is_async = 1';
-$limit= 10000; //控制图片拉取的步长
+$where_data = '  is_async = 1 and pic is not null ';
+$limit= 5000; //控制图片拉取的步长
 $order_by =' order by pro_book_id desc';
 
 if($id){
     $where_data .=" and pro_book_id < ".$id;
+    // $where_data .= " and pro_book_id = 125119";
 }
-$sql = "select pro_book_id,title,author,cover_logo,story_link from ims_novel_info where ".$where_data;
+$sql = "select pro_book_id,i.title,m.author,cover_logo,story_link,m.pic from book_center.ims_novel_info as i left join novel.mc_book m on i.pro_book_id = m.id  where ".$where_data;
 $sql .= $order_by;
 $sql .= " limit ".$limit;
 echo "sql : " .$sql ."\r\n";
 $info = $mysql_obj->fetchAll($sql,'db_slave');
 if(!$info) $info = array();
 $diff_data = array();
-
 if(!empty($info)){
     //获取图片
     foreach($info as $value){
 
         $cover_logo = $value['cover_logo'] ?? '';
-        $title = $value['title'] ?? '';
-        $author = $value['author'] ?? '';
+        $title = trimBlankSpace($value['title']); //去除收尾空格
+        $author = trimBlankSpace($value['author']);//去除收尾空格
         $pro_book_id = intval($value['pro_book_id']);
-        if(!$cover_logo || !$title || !$author) continue;
+        if(!$cover_logo || !$title) continue;
 
         //获取对应的名称信息,作为对象插入进去
-        $img_name = NovelModel::getFirstImgePath($title,$author,$cover_logo ,$pinyin);
+        // $img_name = NovelModel::getFirstImgePath($title,$author,$cover_logo ,$pinyin);
+        $img_name = $value['pic']; //用这个当图片名
         //保存的图片路径信息
-        $save_img_path  =  Env::get('SAVE_IMG_PATH') . DS. $img_name;
-        if(!file_exists($save_img_path)){
-        }else{
-            $diff_data[] =[
-                'title' => $title,
-                'author'    =>$author,
-                'link'      =>  $value['story_link'],
-                'save_img_path'   =>  $save_img_path,
-                'pro_book_id'    =>  $pro_book_id,
-                'cover_logo'    =>  $cover_logo,
-            ];
-        }
+        $save_img_path  =  $img_name;
+        // if(!file_exists($save_img_path)){
+        // }else{
+
+        // }
+        $diff_data[] =[
+            'title' => $title,
+            'author'    =>$author,
+            'link'      =>  $value['story_link'],
+            'save_img_path'   =>  $save_img_path,
+            'pro_book_id'    =>  $pro_book_id,
+            'cover_logo'    =>  $cover_logo,
+        ];
     }
 }else{
     echo "no data\r\n";
