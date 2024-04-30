eb3ddbdd017766bce5d38a2c9ecc3e4548d06086
卢晓峰 <luxiaofeng.200@163.com>
2024-04-30 13:50:37 +0800

    修改配置

--- library/storeModel.php
+++ library/storeModel.php
@@ -202,7 +202,7 @@ class StoreModel{
                     //只要不是404页面的就直接返回，进行组装数据，其他的返回就不需要管了
                     $items[$hostData['path']]=$response->body();
                     // var_dump("strlen =" . strlen($response->body()),"code = " . $response->getStatusCode());
-                    $str ="async child-fork-process num = {$i} url = {$urls[$i]} \t strlen =" . strlen($response->body()) . "\t code = " . $response->getStatusCode();
+                    $str ="async child-fork-process \tnum = {$i} \turl = {$urls[$i]} \tstrlen =" . strlen($response->body()) . " \tcode = " . $response->getStatusCode();
                     echo $str ."\r\n";
                     //判断如果不是200的返回长什么样子
                     if($response->getStatusCode() != 200){
