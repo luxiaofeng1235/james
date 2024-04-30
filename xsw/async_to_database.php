5493ee45bcccfd3ea0336d3a1e7bc52d4c3f426e
卢晓峰 <luxiaofeng.200@163.com>
2024-04-30 18:22:42 +0800

    112

--- xsw/async_to_database.php
+++ xsw/async_to_database.php
@@ -21,67 +21,63 @@ if(!$cateId){
   exit("请输入要处理的分类ID\r\n");
 }
 
-$novel_table_name = Env::get('APICONFIG.TABLE_NOVEL');//小说详情页表信息
-$db_conn = 'db_master';
-
 echo "current page is page = {$page} \r\n";
 echo "*******************************************************\r\n";
 echo "page = {$page} cateId = {$cateId} \r\n";
-$json_file = Env::get('SAVE_CACHE_INFO_PATH') .DS .StoreModel::$detail_page . $cateId . '_'. $page.'.json';
-echo "json_file = $json_file\r\n";
-if(!file_exists($json_file)){
-    return '当前文件不存在，请稍后重试'.PHP_EOL;
-}
-$json_data = readFileData($json_file);
-if(!$json_data) {
-    echo "获取小说分页信息失败\r\n";
-    return false;
-}
-$storyList= json_decode($json_data,true);
-if(!$storyList){
-  echo "暂无小说数据，请稍后重试\r\n";
-  return false;
-}
+saveNovelData($cateId , $page); //同步数据
 
-//更新数据
-//检测当前文件是否存在此本书
-$insertData =[];
-$num = $i_num = 0;
-foreach($storyList as $value){
-    if(!$value) continue;
-    $num++;
-    //清洗对应的数据信息，去空，转义等等，方便进行计算
-   $info = StoreModel::combineNovelHandle($value);
-   //查询是否存在已经同步的小说信息
-   $results  = NovelModel::getNovelByName($info['title'] , $info['author']);
-   if($results){
-       $i_num++;
-       echo "num = {$num} \t title={$results['title']}\t author = {$results['author']} \t 源url = {$results['story_link']} ，exists store_id = {$results['store_id']}  no to run\r\n";
-   }else{
-      $insertData[]=$info;
-      echo "num = {$num} \t title={$value['title']}\t author = {$value['author']} is to insert this data\r\n";
-   }
+function saveNovelData($cateId , $page){
+    $novel_table_name = Env::get('APICONFIG.TABLE_NOVEL');//小说详情页表信息
+    $db_conn = 'db_master';
+    $json_file = Env::get('SAVE_CACHE_INFO_PATH') .DS .StoreModel::$detail_page . $cateId . '_'. $page.'.json';
+    echo "json_file = $json_file\r\n";
+    if(!file_exists($json_file)){
+        return '当前文件不存在，请稍后重试'.PHP_EOL;
+    }
+    $json_data = readFileData($json_file);
+    if(!$json_data) {
+        echo "获取小说分页信息失败\r\n";
+        return false;
+    }
+    $storyList= json_decode($json_data,true);
+    if(!$storyList){
+      echo "暂无小说数据，请稍后重试\r\n";
+      return false;
+    }
+    //更新数据
+    //检测当前文件是否存在此本书
+    $insertData =[];
+    $num = $i_num = 0;
+    foreach($storyList as $value){
+        if(!$value) continue;
+        $num++;
+        //清洗对应的数据信息，去空，转义等等，方便进行计算
+       $info = StoreModel::combineNovelHandle($value);
+       //查询是否存在已经同步的小说信息
+       $results  = NovelModel::getNovelByName($info['title'] , $info['author']);
+       if($results){
+           $i_num++;
+           echo "num = {$num} \t title={$results['title']}\t author = {$results['author']} \t 源url = {$results['story_link']} ，exists store_id = {$results['store_id']}  no to run\r\n";
+       }else{
+          $insertData[]=$info;
+          echo "num = {$num} \t title={$value['title']}\t author = {$value['author']} is to insert this data\r\n";
+       }
+    }
+  echo "\r\n===========共计小说".count($storyList)."本 ，待插入同步的有 (".count($insertData) . ")本，已存在的有 ( {$i_num} )本\r\n";
+  echo "\r\n";
+  echo "*******************************************************\r\n";
+  echo "\r\n";
+  // if($insertData){
+  //     //同步数据
+  //     $ret= $mysql_obj->add_data($insertData,$novel_table_name,$db_conn);
+  //     if(!$ret){
+  //         echo "数据库数据同步失败\r\n";
+  //     }
+  //     echo "同步小说列表成功 \r\n";
+  // }else{
+  //     echo "暂无小说需要插入的数据同步\r\n";
+  // }
+
 }
-
-echo '<pre>';
-print_R($insertData);
-echo '</pre>';
-exit;
-echo "\r\n===========共计小说".count($storyList)."本 ，待插入同步的有 (".count($insertData) . ")本，已存在的有 ( {$i_num} )本\r\n";
-echo "\r\n";
-echo "*******************************************************\r\n";
-echo "\r\n";
-
-// if($insertData){
-//     //同步数据
-//     $ret= $mysql_obj->add_data($insertData,$novel_table_name,$db_conn);
-//     if(!$ret){
-//         echo "数据库数据同步失败\r\n";
-//     }
-//     echo "同步小说列表成功 \r\n";
-// }else{
-//     echo "暂无小说需要插入的数据同步\r\n";
-// }
-
 echo "finish\r\n";
 ?>
