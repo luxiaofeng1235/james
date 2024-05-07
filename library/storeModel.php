88c33a5c40b19d05777001cca437d38f967d13e0
卢晓峰 <luxiaofeng.200@163.com>
2024-05-07 14:11:44 +0800

    12

--- library/storeModel.php
+++ library/storeModel.php
@@ -237,9 +237,9 @@ class StoreModel{
                 //利用屏障的create来开启对应的配置信息
                 Coroutine::create(function () use ($http,$barrier, &$count,$i,&$items,$urls ,$proxy_data) {
                     $response = $http->ua('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0')
-                                     ->rawHeader('ddd:value4')
-                                      ->proxy($proxy_data['ip'], $proxy_data['port'], 'socks5') //认证类型设置
-                                     // ->proxyAuth($proxy_data['username'],$proxy_data['password']) //认证账密
+                                    ->rawHeader('ddd:value4')
+                                    ->proxy($proxy_data['ip'], $proxy_data['port'], 'socks5') //认证类型设置
+                                    // ->proxyAuth($proxy_data['username'],$proxy_data['password']) //认证账密
                                      ->get($urls[$i]);
                     $hostData = parse_url($urls[$i]??'');
                     //只要不是404页面的就直接返回，进行组装数据，其他的返回就不需要管了
